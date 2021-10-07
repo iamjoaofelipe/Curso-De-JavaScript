85815ff66a208e2fcc4c7c86c55bf5f8e8357fae
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-07 19:01:05 -0300

    Atribuir o valor de uma variável a outra variável

---
+++ 04-atribuir-o-valor-de-uma-variável-a-outra-variável.js
@@ -0,0 +1,19 @@
+// Atribuir o valor de uma variável a outra variável.
+
+// Opção 1
+
+var a = 5;
+var b = a;
+
+console.log(a);
+console.log(b);
+
+// Opção 2
+
+var a = 5;
+var b;
+
+b = a;
+
+console.log(a);
+console.log(b);
