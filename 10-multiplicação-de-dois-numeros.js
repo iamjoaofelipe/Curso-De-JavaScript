a3aa53e07f050aeeccd27eb260dfde9732b2bbb4
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 11:30:22 -0300

    Multiplicação de dois números

---
+++ 10-multiplicação-de-dois-numeros.js
@@ -0,0 +1,13 @@
+// Multiplicação de dois números em JavaScript.
+
+var produto = 5 * 3;
+console.log(produto);
+
+var produto = 9 * 0;
+console.log(produto);
+
+var produto = -5 * 6;
+console.log(produto);
+
+var produto = -5 * -3;
+console.log(produto);
