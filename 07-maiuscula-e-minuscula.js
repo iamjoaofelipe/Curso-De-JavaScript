74b034fad26f8667fd10d1357d364d92ae50d006
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 10:36:35 -0300

    MAIÚSCULA e minúscula em JavaScript

---
+++ 07-maiuscula-e-minuscula.js
@@ -0,0 +1,15 @@
+// Diferenciação de MAIÚSCULA e minúscula em JavasSript.
+
+var minhaVariavel = 9;
+
+/*
+ERRADO
+console.log(MINHAVARIAVEL); 
+console.log(Minhavariavel) 
+console.log(MInhavarIAVEL) 
+console.log(minhavariavel) 
+ERRADO
+*/
+
+console.log(minhaVariavel); // CERTO
+
