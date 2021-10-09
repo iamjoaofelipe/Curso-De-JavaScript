95fe8221b589b018aafb710978aba52cd9379a78
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 12:38:07 -0300

    Multiplicar dos números decimais

---
+++ 13-multiplicar-dois-numeros-decimais.js
@@ -0,0 +1,18 @@
+//Multiplicar dos números decimais com JavaScript.
+
+
+//var produto =  3,4  *  10,4 ;
+//console.log(produto);
+
+
+produto = 2, 4 * 4;
+console.log(produto);
+
+produto = 6 * 8, 9;
+console.log(produto);
+
+produto = 3, 6 * 0;
+console.log(produto);
+
+produto = - 5, 7 * 3, 4;
+console.log(produto);
