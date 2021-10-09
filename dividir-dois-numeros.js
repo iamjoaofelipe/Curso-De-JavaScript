ab285022e514849db9ff2fe9e358bb840e0a33a9
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 11:37:36 -0300

    Dividir dois números

---
+++ dividir-dois-numeros.js
@@ -0,0 +1,11 @@
+// Dividir dois números em JavaScript.
+
+
+var  cociente  =  20  /  2 ;
+console . log ( cociente ) ;
+
+cociente  =  17  /  31  ;
+console . log ( cociente ) ;
+
+cociente  =  3  /  0 ;
+console . log ( cociente ) ;
