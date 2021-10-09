cd4780c4d8d88c5d63d037104470b6d152cb80d5
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 20:46:47 -0300

    Calcular o resto de uma divisão

---
+++ 15-calcular-resto-de-uma-divisão.js
@@ -0,0 +1,19 @@
+// Calcular o resto de uma divisão em JavaScript.
+
+var resto = 15 % 5;
+console.log(resto);
+
+resto = 5 % 1;
+console.log(resto);
+
+resto = 5 % 2;
+console.log(resto);
+
+resto = 5 % 3;
+console.log(resto);
+
+resto = 5 % 4;
+console.log(resto);
+
+resto = 5 % 5;
+console.log(resto);
