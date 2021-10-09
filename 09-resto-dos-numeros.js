5b57158c940ad7c2802d5fefafc985678ecf45ee
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 10:59:25 -0300

    Resto dos números

---
+++ 09-resto-dos-numeros.js
@@ -0,0 +1,11 @@
+// Resto dos números em JavaScript.
+
+var resto = 15 - 5; // positivo
+console.log(resto);
+
+resto = 5 - 15; // negativo
+console.log(resto);
+
+resto = 15 - 15; // zero
+console.log(resto);
+
