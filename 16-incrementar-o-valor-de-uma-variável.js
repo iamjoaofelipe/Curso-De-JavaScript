705e236597f1a96da692aa1c6732a5607b7a0efb
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 21:41:21 -0300

    incrementar  o valor de uma variável

---
+++ 16-incrementar-o-valor-de-uma-variável.js
@@ -0,0 +1,14 @@
+// Incrementar o valor de uma variável
+
+var livrosComprados = 11;
+console.log(livrosComprados);
+
+// opção 1
+
+livrosComprados = livrosComprados +1;
+console.log(livrosComprados);
+
+// opção 2
+
+livrosComprados++;
+console.log(livrosComprados);
