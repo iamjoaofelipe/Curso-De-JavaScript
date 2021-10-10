5394c28942d7d578a9aea1036a33fe2c95a92727
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 22:12:03 -0300

    Reduzir o valor de uma variável

---
+++ 17-reduzir-o-valor-de-uma-variável.js
@@ -0,0 +1,13 @@
+// Reduzir o valor de uma variável.
+
+var numeroDeEstudantes = 100;
+console.log(numeroDeEstudantes);
+
+// 1 opção
+numeroDeEstudantes = numeroDeEstudantes - 1;
+console.log(numeroDeEstudantes);
+
+// segunda opção.
+numeroDeEstudantes--;
+console.log(numeroDeEstudantes);
+
