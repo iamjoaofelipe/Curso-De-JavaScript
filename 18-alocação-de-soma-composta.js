1404b29e7abcff73c84cd7b124c0ff5ab789e639
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-09 22:46:28 -0300

    Alocação de soma composta

---
+++ 18-alocação-de-soma-composta.js
@@ -0,0 +1,15 @@
+// Alocação de soma composta em JavaScript.
+
+var a =5;
+
+a = a +5;
+console.log(a);
+
+a += 5;
+console.log(a);
+
+var totalDeVendas = 13567.34;
+console.log(totalDeVendas);
+
+totalDeVendas += 345.67;
+console.log(totalDeVendas);
