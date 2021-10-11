966fa39cd34160641dcf4fd3dec1319af54392a2
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-11 17:51:31 -0300

    Atribuição composta de divisão

---
+++ 21-atribuição-composta-de-divisão.js
@@ -0,0 +1,17 @@
+// Atribuição composta de divisão em JavaScript.
+
+var d = 39;
+
+d = d / 3;
+console.log(d);
+
+d /= 3;
+console.log(d);
+
+// exemplo
+
+var salario = 5000;
+salario /= 2;
+console.log(salario);
+
+
