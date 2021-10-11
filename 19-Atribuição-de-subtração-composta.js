1adf341f65a819c8a3a628a9f310b557d775eed6
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-11 15:05:53 -0300

    Atribuição de subtração composta

---
+++ 19-Atribuição-de-subtração-composta.js
@@ -0,0 +1,14 @@
+// Atribuição de subtração composta em Javascript.
+
+var b = 23;
+b = b - 3;
+console.log(b);
+
+b -= 3;
+console.log(b);
+
+var totalDelta = 2446;
+
+console.log(totalDelta);
+totalDelta -= 345;
+console.log(totalDelta);
