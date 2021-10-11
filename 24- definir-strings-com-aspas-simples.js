39254c87bab2d7bdddc8d8da1f9f04e96fb485f7
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-11 20:57:52 -0300

    Definir strings com aspas simples

---
+++ 24- definir-strings-com-aspas-simples.js
@@ -0,0 +1,12 @@
+// Definir strings com aspas simples em JavaScript.
+
+var minhaMeta ;
+
+minhaMeta = "Aprender a programar em \"JavaScript\"";
+
+// outro tipo de aspas
+
+minhaMeta = 'Aprender a programar em "JavaScript"';
+
+minhaMeta = "aprender a programar em 'JavaScript'";
+console.log(minhaMeta);
