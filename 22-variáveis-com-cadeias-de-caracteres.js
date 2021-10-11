d30bc8c344d727f02dbcffde543239b3feb7eaa0
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-11 19:28:28 -0300

    Variáveis ​​com cadeias de caracteres

---
+++ 22-variáveis-com-cadeias-de-caracteres.js
@@ -0,0 +1,14 @@
+// Variáveis ​​com cadeias de caracteres em JavaScript.
+
+var nome = "Joao";
+
+// variações da cadeia de caracteres
+
+var nome = "Jo a";
+var nome = "J&*%^&@@oao";
+var nome = "Joao1234_+";
+
+// exemplo 
+
+var apelido = "joaozinho";
+
