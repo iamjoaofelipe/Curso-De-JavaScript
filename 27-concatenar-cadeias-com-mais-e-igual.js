b1857d903b08a78ffb0f6bd45b8b67f712485d8a
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 18:24:06 -0300

    Concatenar cadeias com mais e igual

---
+++ 27-concatenar-cadeias-com-mais-e-igual.js
@@ -0,0 +1,8 @@
+// Concatenar cadeias com mais e igual em JavaScript.
+
+var mensagemCompleta = "Estou aprendendo a programar ";
+console.log(mensagemCompleta);
+
+mensagemCompleta += "em JavaScript"; 
+// adicionei uma string ao final da frase
+console.log(mensagemCompleta);
