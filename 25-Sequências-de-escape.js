341afe769b8dbfc0f1cac046aa55110f35397471
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 17:54:20 -0300

    Sequências de escape

---
+++ 25-Sequências-de-escape.js
@@ -0,0 +1,25 @@
+/*   Sequências de escape em JavaScript.
+
+\ 'Aspas simples
+\ "Citação dupla
+\\ Barra invertida
+\ n Nova Linha
+\ r Retorno de carro
+\ t Tab
+\ b Backspace
+\ f Quebra de página
+*/
+
+var minhaMeta;
+
+minhaMeta = "aprender \'JavaScript\'";
+console.log(minhaMeta);
+
+minhaMeta = "Aprender \JavaScript\"";
+console.log(minhaMeta);
+
+minhaMeta = "Aprender \\ JavaScript";
+console.log(minhaMeta);
+
+minhaMeta = "Aprender \n JavaScript";
+console.log(minhaMeta);
