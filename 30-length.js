2675630f90778f1cbb7f1386e048b24792545462
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 19:16:26 -0300

    Essa propriedade retorna o número de unidades

---
+++ 30-length.js
@@ -0,0 +1,21 @@
+// length 
+//Essa propriedade retorna o número de unidades de código presentes na string.
+
+var minhaCadeia;
+
+minhaCadeia = "A";
+console.log(minhaCadeia.length);
+
+minhaCadeia = "AB";
+console.log(minhaCadeia.length);
+
+var linguagemDeProgramação = "JavaScript";
+console.log(linguagemDeProgramação.length);
+
+var mensagem = "Estou aprendendo a programar!";
+console.log(mensagem.length);
+
+var mensagem = "Estou aprendendo a programar @#&*";
+console.log(mensagem.length);
+
+
