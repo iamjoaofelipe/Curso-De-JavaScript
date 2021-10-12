887e425b1115a67621e4e4a6d2623b62ddaa5326
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 19:31:14 -0300

    Notação de parênteses

---
+++ 31-Notação-de-parênteses.js
@@ -0,0 +1,10 @@
+// Notação de parênteses em JavaScript.
+
+var linguagemDeProgramação = "JavaScript";
+
+/*
+      J a v a S c r i p t
+      0 1 2 3 4 5 6 7 8 9
+*/
+
+console.log(linguagemDeProgramação[0]);
