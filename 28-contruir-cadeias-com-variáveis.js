a27b796ae64d9870bb51b4afff40bfb02ec7a2bb
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 19:01:55 -0300

    Construir cadeias com variáveis

---
+++ 28-contruir-cadeias-com-variáveis.js
@@ -0,0 +1,6 @@
+// Contruir cadeias com variáveis em JavaScript.
+
+var verbo = "programar";
+var mensangem = "Estou aprendendo a " + verbo + " em JavaScript";
+console.log(mensangem); 
+
---
+++ 29-Contruir-cadeias-com-variáveis.js
@@ -0,0 +1,9 @@
+// Construir cadeias com variáveis em JavaScript.
+
+var mensagemCompleta = "Estou aprendendo a programar ";
+var parteFinal = "em JavaScript";
+
+console.log(mensagemCompleta);
+
+mensagemCompleta += parteFinal;
+console.log(mensagemCompleta);
