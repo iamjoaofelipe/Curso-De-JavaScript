97aef94248ed6170bfe2644bd40117b18877687f
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-12 18:14:06 -0300

    Concatenar cadeias de caracteres

---
+++ 26-concatenar-cadeias-de-caracteres.js
@@ -0,0 +1,18 @@
+// Concatenar cadeias de caracteres em JavaScript.
+
+var nomeCompleto = "João" + "Felipe";
+// Sem espaço
+console.log(nomeCompleto);
+
+var nomeCompleto = "João " + "Felipe";
+// Com espaço
+console.log(nomeCompleto);
+
+var nomeCompleto = "João" + " Felipe";
+// Com espaço
+console.log(nomeCompleto);
+
+var nomeCompleto = "João" + " " +"Felipe"; 
+// Com espaço
+console.log(nomeCompleto);
+
