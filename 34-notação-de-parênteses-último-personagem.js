6480c60dac9e536dadc18f051b2aeab1c040a063
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 10:46:16 -0300

    Notação de parênteses,último personagem - 1

--- 34-notação-de-parênteses-último-personagem.js
+++ 34-notação-de-parênteses-último-personagem.js
@@ -3,7 +3,6 @@
 var minhaCorrente 
 
 /*
-
 O último índice é sempre longo - 1 porque começamos a contar a partir de 0. 
 contando a partir de 0.
 myString.length para "JavaScript" é 10.
@@ -11,7 +10,6 @@ O último índice é 9.
 
 Corrente : J a v a S c r i p t 
 Índices  : 0 1 2 3 4 5 6 7 8 9     
-
 */
 
 minhaCorrente = "JavaScript";
