cbbfb2b6b4b40cc956bf3138b222f6a359385e75
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 13:01:37 -0300

    Palavras em branco

---
+++ 36-palavras-em-branco.js
@@ -0,0 +1,16 @@
+// Palavras em branco em JavaScript.
+
+var miSustantivo = "cachorro";
+var miAdjetivo = "preto";
+var miVerbo = "correu";
+var miAdverbio = "rápidamente";
+
+// O cachorro preto correu rapidamente para a tenda.
+/* Concatena as cordas e cria uma nova corda que exibe uma mensagem. 
+Você pode alterar os valores das variáveis.
+Por exemplo: O cachorro preto correu rapidamente para a loja.
+             A pequena bicicleta voou para a loja lentamente.
+*/
+
+var palavrasEmBranco = "O " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " " + miAdverbio + " para casa.";
+console.log(palavrasEmBranco); 
