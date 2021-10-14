c67280adb01c225da524eacecb6d730b842870cc
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 11:05:50 -0300

    Notação de parênteses: da direita para a esquerda

---
+++ 35-notação-de-parênteses-da-direita-para-a-esquerda.js
@@ -0,0 +1,16 @@
+// Notação de parênteses: da direita para a esquerda em JavaScript.
+
+/*
+Corrente : J a v a S c r i p t 
+Índices  : 0 1 2 3 4 5 6 7 8 9     
+*/
+
+minhaCorrente = "JavaScript";
+console.log(minhaCorrente[minhaCorrente.length-2]); // p
+
+// alternativa
+
+minhaCorrente = "JavaScript";
+n = 4;
+
+console.log(minhaCorrente[minhaCorrente.length-n]); // da direita para a esquerda
