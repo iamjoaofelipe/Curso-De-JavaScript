2f5afe1546fa6ea7741c5eb60b4ff5d04215783e
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 13:33:01 -0300

    Arranjos

---
+++ 37-Arranjos.js
@@ -0,0 +1,10 @@
+// Arranjos em JavaScript.
+
+var meuArranjo = ["João", 100];
+console.log(meuArranjo);
+
+var estudantes = ["Bruno", "Leandro", "Robson", "Wellington"];
+console.log(estudantes);
+
+var notas = [99, 98, 97, 96];
+console.log(notas);
