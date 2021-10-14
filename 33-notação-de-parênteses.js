8109dc24abaa65a877cf695afe1c68ad377c6694
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 10:28:41 -0300

    Notação de parênteses

---
+++ 33-notação-de-parênteses.js
@@ -0,0 +1,31 @@
+// Notação de parênteses em JavaScript.
+
+var minhaCorrente = "JavaScript";
+
+/*
+      J a v a S c r i p t
+      0 1 2 3 4 5 6 7 8 9
+*/
+
+console.log(minhaCadeia[0]); // J
+console.log(minhaCadeia[1]); // a
+console.log(minhaCadeia[2]); // v
+console.log(minhaCadeia[3]); // a
+console.log(minhaCadeia[4]); // S
+console.log(minhaCadeia[5]); // c
+console.log(minhaCadeia[6]); // r
+console.log(minhaCadeia[7]); // i
+console.log(minhaCadeia[8]); // p
+console.log(minhaCadeia[9]); // t
+
+// exemplo
+
+var minhaCorrente = "programar";
+
+/*
+corrente : P r o g r a m a r  
+índices  : 0 1 2 3 4 5 6 7 8
+
+*/
+
+console.log(minhaCorrente[0]);
