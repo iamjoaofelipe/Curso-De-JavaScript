28bcff4ffac26a55043c968541d7f2a9ad63d996
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-14 14:46:38 -0300

    Acesso aos elementos de uma matriz

---
+++ 39-acesso-aos-elementos-de-uma-matriz.js
@@ -0,0 +1,14 @@
+// Acesso aos elementos de uma matriz em JavaScript.
+
+var meuAcordo = [10, 20 , 30];
+/*
+acordo [ 10 , 20 , 30]
+índices   0    1    2
+*/
+
+console.log(meuAcordo[0]); // 10 
+console.log(meuAcordo[1]); // 20
+console.log(meuAcordo[2]); // 30
+
+var soma = meuAcordo[0] + meuAcordo[1] + meuAcordo[2];
+console.log(soma);  
