18057fc5a333c6abd7435330339eb5a91866eab0
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-15 14:53:45 -0300

    pop

---
+++ 43-pop.js
@@ -0,0 +1,9 @@
+// Manipulação de array com .pop(); 
+// O método pop() remove o último elemento de um array e retorna aquele elemento.
+
+var estações;
+
+estações = ["inverno" , "outono" , "primavera" , "verão"];
+estação = estações.pop();
+console.log(estação);
+console.log(estações);
