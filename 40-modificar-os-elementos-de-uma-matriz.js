8b3ccd36237e8784ee6722671167127f11e28f44
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-15 10:35:13 -0300

    Modificar os elementos de uma matriz

---
+++ 40-modificar-os-elementos-de-uma-matriz.js
@@ -0,0 +1,13 @@
+// Modificar os elementos de uma matriz em JavaScript.
+
+var minhaMatriz = [10,20,30];
+
+console.log(minhaMatriz);
+minhaMatriz[0] = 40;
+console.log(minhaMatriz);
+
+minhaMatriz[1] = "Hola";
+console.log(minhaMatriz);
+
+minhaMatriz [1] = [1,2,3];
+console.log(minhaMatriz);
