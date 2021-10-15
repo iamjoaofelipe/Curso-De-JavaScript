3c57034f902ff12e986a3fb358505832da084598
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-15 14:24:33 -0300

    Push

---
+++ push.js
@@ -0,0 +1,7 @@
+// Manipular Array com push em Javascript.
+
+var estações = ["inverno", "outono" ,"primavera"]; // ordem alfabética
+console.log(estações);
+estações.push("verão");
+// adicionei verão ao final da Array
+console.log(estações);
