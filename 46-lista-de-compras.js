5292f36ada78a599e38e195aa6e9a8b42c45e07c
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-18 14:44:19 -0300

    Lista de compras

---
+++ 46-lista-de-compras.js
@@ -0,0 +1,10 @@
+// Lista de compras em JavaScript.
+
+var minhaListaDeCompras = [["cereal", 3], ["leite", 2], ["bolachas", 4], ["pão", 5], ["refresco", 7], ["frango", 7]];
+
+console.log("Eu vou comprar " + minhaListaDeCompras[0][1] + " unidades de " + minhaListaDeCompras[0][0] + ".");
+console.log("Eu vou comprar " + minhaListaDeCompras[1][1] + " unidades de " + minhaListaDeCompras[1][0] + ".");
+console.log("Eu vou comprar " + minhaListaDeCompras[2][1] + " unidades de " + minhaListaDeCompras[2][0] + ".");
+console.log("Eu vou comprar " + minhaListaDeCompras[3][1] + " unidades de " + minhaListaDeCompras[3][0] + ".");
+console.log("Eu vou comprar " + minhaListaDeCompras[4][1] + " unidades de " + minhaListaDeCompras[4][0] + ".");
+console.log("Eu vou comprar " + minhaListaDeCompras[5][1] + " unidades de " + minhaListaDeCompras[5][0] + ".");
