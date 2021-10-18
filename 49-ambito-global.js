65f85c4c0a375082916d0e5421dc2de5e9492fc8
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-18 15:31:11 -0300

    Âmbito global

---
+++ 49-ambito-global.js
@@ -0,0 +1,10 @@
+// Ambito global em JavaScript.
+
+var minhaVariavel = 5;
+console.log(minhaVariavel);
+
+function minhaFunção() {
+    console.log(minhaVariavel);
+}
+
+console.log(minhaVariavel);
