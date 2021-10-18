73ad4ae49ff13529642ed9225b3b2319f30f8403
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-18 15:00:27 -0300

    Funções

---
+++ 47-funções.js
@@ -0,0 +1,11 @@
+// Funções em JavaScript.
+
+function mostrarMensagem() {
+    console.log("Olá,Mundo!");
+}
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
