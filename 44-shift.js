7d3965c795e408c5e2adf44f1123472645c36853
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-18 11:50:14 -0300

    Remove o primeiro elemento de um array

---
+++ 44-shift.js
@@ -0,0 +1,8 @@
+// O método shift()remove o primeiro elemento de um array e retorna esse elemento.
+//Este método muda o tamanho do array. 
+
+var estacões = ["inverno" , "outono" , "primavera"];
+
+estacões.shift();
+
+console.log(estacões);
