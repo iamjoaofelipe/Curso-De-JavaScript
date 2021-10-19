c4a161ce9373242e5a6132a9fa9bd2ec000afa8e
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-19 16:09:46 -0300

    Escopo global e variáveis ​​globais

---
+++ 51-Escopo-global-e-variáveis -globais.js
@@ -0,0 +1,11 @@
+// Escopo global e variáveis ​​globais em JavaScript.
+
+var meuNome = "João";
+
+function mostrarMeuNome() {
+    var meuNome = "Felipe";
+    console.log(meuNome);
+    
+}
+mostrarMeuNome();
+console.log(meuNome);
