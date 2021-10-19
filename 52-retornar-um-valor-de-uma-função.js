d99899d3184dd39cefc4d9c53ad5d282479ffdde
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-19 16:31:39 -0300

    Retornar o valor de uma função

---
+++ 52-retornar-um-valor-de-uma-função.js
@@ -0,0 +1,11 @@
+// Retornar o valor de uma função em JavaScript.
+
+function somar(a, b) {
+    return a + b;
+}
+
+somar(5, 3);
+// Nada é exibido no console
+
+console.log(somar(5, 3));
+// O valor retornado é exibido
