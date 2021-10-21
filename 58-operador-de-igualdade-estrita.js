5ac3f02150c33247981b7ffe51314989398abb46
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-21 16:13:10 -0300

    Pratique comparar valores

---
+++ 58-operador-de-igualdade-estrita.js
@@ -0,0 +1,9 @@
+// Operador de igualdade estrita em JavaScript.
+
+console.log(9 == 9); // true
+
+console.log(9 == "9"); // true
+
+console.log(9 === 9); // true
+
+console.log(9 === "9"); // false
