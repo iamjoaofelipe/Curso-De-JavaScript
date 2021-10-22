53f3502a9f130cff83986c159548a5ec7ce9aca0
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-22 13:21:16 -0300

    Operador menor que

---
+++ 64-operador-menor-que.js
@@ -0,0 +1,15 @@
+// Operador menor que em JavaScript.
+
+// menor que: <
+
+console.log(5 < 6); // true
+console.log(10 < 6); // false
+
+console.log("A" < "B");// true
+console.log("ABC" < "ACB"); // true
+
+var a = 15;
+var b = 7;
+
+console.log(a < b); //false
+console.log(b < a); //true
