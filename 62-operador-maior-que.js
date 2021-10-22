7f028e84ab70f37f834aa1667f6240e63e8faa26
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-22 12:58:36 -0300

    Operador maior que

--- 62-operador-maior-que.js
+++ 62-operador-maior-que.js
@@ -1,2 +1,22 @@
 // Operador maior que em JavaScript.
 
+console.log(6 > 5); // true
+console.log(3 > 10) // false
+
+console.log("B" > "A"); // true
+console.log("ACB" > "ABC"); // false
+
+console.log("AB" > "A"); true
+
+console.log("Mundo" > "Hola"); // true
+
+console.log("M" > "A"); // true
+
+console.log("JavaScript" > "Python"); // false
+
+console.log( "J" > "P"); // false
+
+var a = 15;
+var b = 7;
+console.log( a > b ); //true
+console.log( b > a ); //false
