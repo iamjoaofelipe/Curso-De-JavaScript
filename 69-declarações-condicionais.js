fb7f138ac1bd04aeec631b276e07be3d7809e4f2
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-25 19:27:31 -0300

    IF

---
+++ 69-declarações-condicionais.js
@@ -0,0 +1,26 @@
+// Declarações condicionais em JavaScript.
+
+if (true) {
+    console.log("Essa condição é verdadeira");
+
+}
+
+// condição verdadeira
+
+var x = 5;
+if (x > 2) {
+    console.log("Essa condição é verdadeira");
+}
+
+if (x > 2 && x < 10) {
+    console.log("Essa condição é verdadeira");
+}
+
+// exemplo
+
+var estação = "inverno";
+if (estação == "inverno") {
+    console.log("sim! Eu amo o inverno");
+}
+//console.log("Depois da condição...");
+
