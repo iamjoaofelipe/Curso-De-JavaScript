4310a7c0bb0bf433b0d8203c5ff1d78859b07e0d
iamjoaofelipe <79280942+iamjoaofelipe@users.noreply.github.com>
2021-10-28 11:48:11 -0300

    Condicionais, ordem lógica

---
+++ 72-condicionais-ordem-lógica.js
@@ -0,0 +1,14 @@
+// Condicionais, ordem lógica em Javascript.
+
+function classificarValor(valor) {
+    if (valor < 5) {
+        console.log("Menor que 5.");
+    } else if (valor < 10) {
+        console.log("Maior que 10.");
+    } else {
+        console.log("Maior ou igual a 10.");
+    }
+}
+classificarValor(7);
+classificarValor(2);
+classificarValor(15);
