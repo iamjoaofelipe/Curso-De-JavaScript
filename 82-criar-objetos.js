32b271d9dc4b7a0c26048ba7a23619babdae2bb8
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-04 13:46:33 -0300

    Criar objetos

---
+++ 82-criar-objetos.js
@@ -0,0 +1,16 @@
+// Criar objetos em  JavaScript.
+
+var meuCachorro = {
+    "nome": "Kiara",
+    "idade": 5,
+    "peso": 6,
+    "raça": "vira-lata",
+  };
+  
+  var miObjeto = {
+    5: "cinco"
+  };
+ 
+
+
+
