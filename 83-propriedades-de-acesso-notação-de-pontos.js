878b07fb882fa3519ad652d0169ddc170afc77cf
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-04 14:05:09 -0300

    Propriedades de acesso: notação de pontos

---
+++ 83-propriedades-de-acesso-notação-de-pontos.js
@@ -0,0 +1,19 @@
+// Propriedades de acesso: notação de pontos em JavaScript.
+
+var meuCachorro = {
+"nome": "Kiara",
+"idade": 5,
+"peso": 6,
+"raça": "vira-lata",
+
+};
+
+var meuObjeto = {
+
+};
+
+console.log(meuCachorro.nome); 
+console.log(meuCachorro.idade);
+console.log(meuCachorro.peso);
+console.log(meuCachorro.raça);
+
