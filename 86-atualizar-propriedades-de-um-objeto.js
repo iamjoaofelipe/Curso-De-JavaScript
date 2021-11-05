9e4f1cc089bc368ffa13c58fa2ba5577f74975da
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-05 09:29:10 -0300

    atualizar com push

---
+++ 86-atualizar-propriedades-de-um-objeto.js
@@ -0,0 +1,22 @@
+// Atualizar propriedades de um objeto em JavaScript.
+
+var mochila = {
+    "cor": "azul",
+    "tamanho": "médio",
+    "conteúdo": ["garrafa de água", "caderno"]
+  };
+  
+  console.log(mochila.cor); // azul
+  mochila.cor = "verde";
+  console.log(mochila.color); // verde
+  
+  console.log(conteúdo);
+  mochila.conteúdo.push("lápis");
+  console.log(conteúdo);
+  
+  mochila.conteúdo = [];
+  console.log(mochila.conteúdo); 
+
+  
+
+
