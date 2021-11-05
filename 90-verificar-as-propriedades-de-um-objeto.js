253aa0863c7d444b2dd5d92f0930b297bb34a283
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-05 15:43:39 -0300

    Verificar propriedades de um objeto

--- 90-verificar-as-propriedades-de-um-objeto.js
+++ 90-verificar-as-propriedades-de-um-objeto.js
@@ -1,9 +1,9 @@
 // Verificar as propriedades de um objeto em JavaScript.
 
 var meuCaderno = {
-"cor": "verde", 
-"categoria": 3,
-"preço":4.56
+    "cor": "verde",
+    "categoria": 3,
+    "preço": 4.56
 };
 
 console.log(meuCaderno.hasOwnProperty("cor")); // true
@@ -12,5 +12,13 @@ console.log(meuCaderno.hasOwnProperty("origem")); // false
 // exemplo
 
 function verificarPropriedade(obj, propriedade) {
-    
+    if (obj.hasOwnProperty(propriedade)) {
+        return "propriedade: " + obj[propriedade];
+
+    } else {
+        return " Esse objeto não tem na propriedade";
+    }
 }
+
+console.log(verificarPropriedade(meuCaderno, "cor"));
+console.log(verificarPropriedade(meuCaderno, "origem"));
