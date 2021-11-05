c6aa6858ea9cd27ee67599e187c5fcfa4ca14681
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-05 09:43:54 -0300

    Adicionando novas propriedades

---
+++ 87-adiciona-novas-propriedades-a-um-objeto.js
@@ -0,0 +1,19 @@
+// Adiciona novas propriedades a um objeto JavaScript. 
+
+var curso = {
+
+"título": "Aprender JavaScript do zero",
+"idioma": "português", 
+"duração": 30
+};
+
+// notação de ponto
+
+console.log(curso.vistas); // indefinido
+curso.vistas = 34500;
+console.log(curso.vistas);
+
+// notação de colchetes
+
+curso ["vistas"] = 34500;
+console.log(curso.vistas); 
