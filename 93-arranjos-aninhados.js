bac009dabd390d9922e4350e060a2b09a127355c
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-09 15:00:54 -0300

    Arrays alinhados

---
+++ 93-arranjos-aninhados.js
@@ -0,0 +1,29 @@
+// arrays alinhados em JavaScript.
+
+var minhasPlantas = [
+    {
+    tipo: "flores",
+    lista: [
+        "rosas",
+        "túlipas",
+        "dentes-de-leão",
+    ]
+},
+
+{
+    tipo: "arvores",
+    lista: [
+        "abeto",
+        "pinheiro",
+        "betula",
+
+    ]
+}
+];
+
+var primeiraFlor = minhasPlantas[0].lista[0];
+console.log(primeiraFlor);
+
+var segundoArboles = minhasPlantas[1].lista[1];
+console.log(segundoArboles);
+
