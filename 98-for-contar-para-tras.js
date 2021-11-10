930acdbc59f1ba5453c1c5fd1654c1e5f67f4bea
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-10 15:22:31 -0300

    for , contar para trás

---
+++ 98-for-contar-para-tras.js
@@ -0,0 +1,17 @@
+// ciclo "for" contar para trás.
+
+//exemplo 
+
+for (var i = 15; i >= 10; i--) {
+ console.log(i);
+
+}
+ var minhaArray = []; 
+
+for ( i = 10; i > 0; i -= 2) {
+
+    minhaArray.push(i);
+}
+
+console.log(minhaArray);
+
