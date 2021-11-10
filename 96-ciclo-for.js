54604bea7061f6e36ef41ca1f2cc60d0d17f3500
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-10 13:40:03 -0300

    for

---
+++ 96-ciclo-for.js
@@ -0,0 +1,22 @@
+// Ciclo "for" em JavaScript.
+
+// exemplo 1
+
+var meuArray = [];
+
+for (var i = 0; i <10; i++ ) {
+    meuArray.push(i);
+
+}
+
+console.log(meuArray);
+
+// exemplo 2 
+
+var meuArray = [];
+
+for ( var i = 0; i < 10; i +=2) {
+meuArray.push("A")
+}
+
+console.log(meuArray);
