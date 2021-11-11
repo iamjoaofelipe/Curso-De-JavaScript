c4abdcc8a60c84d0d392f91fa9b35347a8264955
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:34:09 -0300

    Incrementar valor++.

---
+++ aulas 1-50/16-incrementar-o-valor-de-uma-variável.js
@@ -0,0 +1,11 @@
+// Increment (++) o valor de uma variável em JavaScript.
+var incrementarValor = 105;
+console.log(incrementarValor); 
+// opção 1.
+incrementarValor = incrementarValor + 1;
+console.log(incrementarValor);
+// opção 2.
+incrementarValor++;
+console.log(incrementarValor);
+
+
