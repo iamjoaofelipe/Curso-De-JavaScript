7d9ea0ef6451f2295474206ecff7f4332036bf41
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:18:24 -0300

    Divisão de dois números decimais.

--- 14-dividir-dois-numeros-decimais.js
+++
@@ -1,9 +0,0 @@
-//Dividir dois números decimais em JavaScript.
-var  cociente  =  5.0 /  2.0;  
-console . log ( cociente ) ;
-
-cociente  =  2,3  /  6,7 ;
-console . log ( cociente ) ;
-
-cociente  =  4,2  /  0,0 ;
-console . log ( cociente ) ;
---
+++ aulas 1-50/14-divisão-de-dois-números-decimais.js
@@ -0,0 +1,7 @@
+//Dividir de dois números decimais em JavaScript.
+var dividirDoisNumerosDecimais = 5.0 / 2.0;
+console.log(dividirDoisNumerosDecimais); 
+dividirDoisNumerosDecimais = 2.3 / 6.7;
+console.log(dividirDoisNumerosDecimais); 
+dividirDoisNumerosDecimais = 4.2 / 0.0;
+console.log(dividirDoisNumerosDecimais); 
