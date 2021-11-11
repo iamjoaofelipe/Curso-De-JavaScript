f14a80560b7f44c8d2cebf9d6574bebdc03fa948
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 09:57:23 -0300

    for aninhados

---
+++ 100-ciclo-de-aninhamento.js
@@ -0,0 +1,17 @@
+// ciclos "for" aninhados em JavaScript.
+
+var meuArray = [[1,2,3] , [4,5,6,] , [7,8,9]]; 
+
+//Para cada matriz aninhada
+
+for ( var i = 0; i < meuArray.length; i++) {
+    var aninhado = meuArray[i];
+    // Para cada elemento da matriz aninhada
+
+     for (var j = 0; j < fila.length; j++) {
+        console.log(aninhado[j]); 
+     }
+}
+
+
+
