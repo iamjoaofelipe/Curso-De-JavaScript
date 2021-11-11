ab49dfc0deea3d8b2aa4f8a9ecdbfda01a2ed64b
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:48:04 -0300

    Soma composta.

---
+++ 18-soma-composta.js
@@ -0,0 +1,11 @@
+// Soma composta em JavaScript.
+var a = 10;
+console.log(a);
+a = a + 10;
+console.log(a); 
+var totalDeVendas = 100;
+console.log(totalDeVendas);
+totalDeVendas += 100;
+console.log(totalDeVendas);
+
+
