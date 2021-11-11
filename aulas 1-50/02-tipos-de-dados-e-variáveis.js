2ec79ae7323f98beb088fb79e981445073eede73
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 15:17:27 -0300

    Alguns tipos de dados

--- aulas 1-50/02-tipos-de-dados-e-variáveis.js
+++ aulas 1-50/02-tipos-de-dados-e-variáveis.js
@@ -1,9 +1,6 @@
-// Tipos de dados em JavaScript.
-
+// Alguns tipos de dados em JavaScript.
 /*
-
- undefined, null, boolean, string, symbol, number, object..
-
+undefined, null, boolean, string, symbol, number, object..
 undefined representa um valor indefinido.
 null é um literal em JavaScript que representa um valor nulo ou "vázio".
 O objeto Boolean é um objeto wrapper para um valor booleano. verdadeiro ou falso.
@@ -11,12 +8,9 @@ O objeto global "String"  é um construtor para strings, ou uma sequência de "c
 A função  Symbol() retorna um valor do tipo símbolo (símbolo)
 O objeto JavaScript Number é um objeto encapsulado que permite você trabalhar com valores numéricos.
 Um objeto Number é criado utilizando o construtor Number().
-
 Objeto é uma coleção de propriedades, e uma propriedade é uma associação entre um nome (ou chave) e um valor.
 Um valor de propriedade pode ser uma função, que é então considerada um método do objeto.
-
 */
-
 var minhaVariável = "FreeCodeCamp";
 console.log(minhaVariável);
 // alterar o valor da variável.
