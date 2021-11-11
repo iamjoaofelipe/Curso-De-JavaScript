fca55d68a915a73e8a23490478cc198aadc5f8a4
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 15:57:05 -0300

    Multiplicando dois números.

---
+++ aulas 1-50/10-multiplicando-dois-números.js
@@ -0,0 +1,13 @@
+// Multiplicando dois números em JavaScript.
+var multiplicação = 5 * 3;
+console.log(multiplicação);
+multiplicação = 9 * 0;
+console.log(multiplicação);
+multiplicação = -5 * 6;
+console.log(multiplicação);
+multiplicação = 5 * 6;
+console.log(multiplicação);
+multiplicação = -5 * -6;
+console.log(multiplicação);
+
+
