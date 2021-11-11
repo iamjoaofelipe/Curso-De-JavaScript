f25bcf28ec9e34f878888f020ef15754efff142a
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 15:33:19 -0300

    Iniciação de variáveis

--- 05-inicialização-de-variáveis.js
+++
@@ -1,6 +0,0 @@
-// Iniciação de variáveis em JavaScript.
-var x = 7;
-
-var miIdioma = "Português";
-
-console.log(miIdioma);
---
+++ aulas 1-50/05-inicialização-de-variáveis.js
@@ -0,0 +1,3 @@
+// Iniciação de variáveis em JavaScript.
+var x =9;
+var meuIdioma = "Português";
