d398f8d16f6c6d014fa2f0a1216ef436ce88bae9
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 10:05:11 -0300

    While ciclos

---
+++ 101-ciclos-do-while.js
@@ -0,0 +1,17 @@
+// Ciclos do "while" em JavaScript.
+
+var x = 16;
+
+do {
+    console.log(x < 10 );
+    x++;
+} while ( x > 10);
+
+x = 16;
+
+while ( x > 10) {
+    console.log(x); 
+    x++;
+}
+
+
