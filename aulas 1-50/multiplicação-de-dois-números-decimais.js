610b0874ce03c5cc3c27fdba333d4aab27a26489
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:13:22 -0300

    Multiplicação de dois números decimais.

---
+++ aulas 1-50/multiplicação-de-dois-números-decimais.js
@@ -0,0 +1,12 @@
+//Multiplicação de dois números decimais em JavaScript.
+var multiplicarDoisNumerosDecimais = 3.4 * 10.4;
+console.log(multiplicarDoisNumerosDecimais); 
+multiplicarDoisNumerosDecimais = 2.4 * 4;
+console.log(multiplicarDoisNumerosDecimais); 
+multiplicarDoisNumerosDecimais = 6 * 8.9;
+console.log(multiplicarDoisNumerosDecimais); 
+multiplicarDoisNumerosDecimais = 3.6 * 0;
+console.log(multiplicarDoisNumerosDecimais); 
+multiplicarDoisNumerosDecimais = -5.7 * 3.4;
+console.log(multiplicarDoisNumerosDecimais); 
+
