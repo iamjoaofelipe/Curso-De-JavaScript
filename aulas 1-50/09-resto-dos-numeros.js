f4e2693d6e5b93d6ea8966b64669fbf9ee7504a7
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 15:52:42 -0300

    Resto da subtração.

---
+++ aulas 1-50/09-resto-dos-numeros.js
@@ -0,0 +1,10 @@
+// Resto da subtração em JavaScript.
+var resto = 15 - 3; // número positivo.
+console.log(resto);
+resto = 5 - 15;
+console.log(resto); // número negativo.
+resto = 15 - 15; // igual a zero.
+console.log(resto);
+
+
+
