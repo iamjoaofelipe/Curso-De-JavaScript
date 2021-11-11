b0c09bbd2767e63432fd2c9b4be44a72911e425d
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:58:34 -0300

    Subtração composta com atribuição.

---
+++ aulas 1-50/19-subtração-composta-com-atribuição.js
@@ -0,0 +1,16 @@
+// Substração composta com atribuição em JavaScript.
+var b = 10;
+console.log(b);
+b = b - 3;
+console.log(b);
+b -= 3;
+console.log(b);
+// exemplo.
+var totalDeVendas = 100;
+console.log(totalDeVendas);
+totalDeVendas =- 50;
+console.log(totalDeVendas); 
+
+
+
+
