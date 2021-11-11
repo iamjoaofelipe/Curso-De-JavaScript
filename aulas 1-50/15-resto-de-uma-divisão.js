ba3f9b7c5993de36a158250f96d5b2bd6f80c007
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-11 16:26:08 -0300

    Resto de uma divisão.

--- aulas 1-50/15-resto-de-uma-divisão.js
+++ aulas 1-50/15-resto-de-uma-divisão.js
@@ -1,19 +1,13 @@
-// Calcular o resto de uma divisão em JavaScript.
-
+// Resto de uma divisão em JavaScript.
 var resto = 15 % 5;
 console.log(resto);
-
 resto = 5 % 1;
 console.log(resto);
-
 resto = 5 % 2;
 console.log(resto);
-
 resto = 5 % 3;
 console.log(resto);
-
 resto = 5 % 4;
 console.log(resto);
-
 resto = 5 % 5;
 console.log(resto);
