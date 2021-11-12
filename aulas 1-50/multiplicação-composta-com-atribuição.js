64d478d26e63e3e931871de18c58c93712389eea
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 11:57:09 -0300

    Multiplicação composta com atribuição.

---
+++ aulas 1-50/multiplicação-composta-com-atribuição.js
@@ -0,0 +1,12 @@
+// Multiplicação composta com atribuição em JavaScript.
+var c = 10;
+c = c * 2; 
+console.log(c); 
+c *= 2;
+console.log(c); 
+// exemplo.
+var meuSalário = 4500;
+console.log(meuSalário);
+meuSalário *=2;
+console.log(meuSalário);
+
