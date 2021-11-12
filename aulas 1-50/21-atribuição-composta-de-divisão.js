8e4af0bb36a905acc49e4faf5f99422a4ab18376
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 13:11:04 -0300

    Divisão composta com atribuição.

---
+++ aulas 1-50/21-atribuição-composta-de-divisão.js
@@ -0,0 +1,17 @@
+// Divisão composta com atribuição em JavaScript.
+var divisão = 20;
+divisão = divisão / 2;
+console.log(divisão);
+divisão  /= 2;
+console.log(divisão);
+// exemplo.
+var meuSalário = 4500;
+console.log(meuSalário);
+meuSalário /= 2;
+console.log(meuSalário);
+
+
+
+
+
+ 
