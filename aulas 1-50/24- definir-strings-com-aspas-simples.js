652e1d4e1130081babf63a2c01d537c059f9d573
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 14:43:47 -0300

    "strings".

---
+++ aulas 1-50/24- definir-strings-com-aspas-simples.js
@@ -0,0 +1,10 @@
+// Aspas em JavaScript.
+var meuObjetivo = "Aprender a programar em JavaScript com \"freeCodeCamp\"";
+console.log(meuObjetivo);
+// variações.
+var meuObjetivo = 'Aprender a programar em JavaScript com "freeCodeCamp"'; 
+console.log(meuObjetivo);
+var meuObjetivo = "Aprender a programar em JavaScript com 'freedCodeCamp'";
+console.log(meuObjetivo);
+
+
