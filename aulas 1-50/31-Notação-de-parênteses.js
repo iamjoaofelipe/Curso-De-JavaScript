4f3349892b2e028240f9dd656f0bf4cdcc640ba6
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 16:10:12 -0300

    Notação.

--- aulas 1-50/31-Notação-de-parênteses.js
+++ aulas 1-50/31-Notação-de-parênteses.js
@@ -1,10 +1,9 @@
-// Notação de parênteses em JavaScript.
-
+// Notação em JavaScript.
 var linguagemDeProgramação = "JavaScript";
-
+console.log(linguagemDeProgramação);
 /*
       J a v a S c r i p t
       0 1 2 3 4 5 6 7 8 9
 */
-
 console.log(linguagemDeProgramação[0]);
+
