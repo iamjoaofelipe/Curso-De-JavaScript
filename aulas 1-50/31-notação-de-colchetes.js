4b611e5a7ef4fa2cfec383f9a9b112c08147559e
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 16:21:43 -0300

    Notação de colchetes.

--- aulas 1-50/31-notação-de-colchetes.js
+++ aulas 1-50/31-notação-de-colchetes.js
@@ -1,4 +1,4 @@
-//  em JavaScript.
+// Notação de colchetes em JavaScript.
 var linguagemDeProgramação = "JavaScript";
 console.log(linguagemDeProgramação);
 /*
@@ -6,4 +6,5 @@ console.log(linguagemDeProgramação);
       0 1 2 3 4 5 6 7 8 9
 */
 console.log(linguagemDeProgramação[0]);
+
 
