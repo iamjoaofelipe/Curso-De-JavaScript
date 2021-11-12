db9c71d8b94090b56307ffd289e4ef005aa52041
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 15:50:13 -0300

    Concatenação.

--- aulas 1-50/28-concatenação.js
+++ aulas 1-50/28-concatenação.js
@@ -1,8 +1,9 @@
 // Concatenação em JavaScript.
 var umaPalavra = "programar";
 console.log(umaPalavra);
-var UmaFraseIncompleta = "Estou aprendendo a " + umaPalavra + " em JavaScript com freeCodeCamp!";
-console.log(UmaFraseIncompleta);
+var umaFraseIncompleta = "Estou aprendendo a " + umaPalavra + " em JavaScript com freeCodeCamp!";
+console.log(umaFraseIncompleta);
+
 
 
 
