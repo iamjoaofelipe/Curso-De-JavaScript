b185924441ff4962f8013e304f55cc1c89b7a8fc
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 15:07:18 -0300

    Strings.

---
+++ aulas 1-50/25-Sequências-de-escape.js
@@ -0,0 +1,18 @@
+// "Strings" em JavaScript.
+/*
+Código | Resultado
+------------------  
+\'      aspas simples
+\"      aspas duplas
+\\      Barra invertida
+\n      pular uma linha
+*/
+var meuObjetivo = "Aprender \'JavaScript\'"; 
+console.log(meuObjetivo); 
+var meuObjetivo = "Aprender \"JavaScript\"";
+console.log(meuObjetivo); 
+var meuObjetivo = "Aprender \\JavaScript"; 
+console.log(meuObjetivo); 
+var meuObjetivo = "Aprender \n JavaScript";
+console.log(meuObjetivo); 
+
