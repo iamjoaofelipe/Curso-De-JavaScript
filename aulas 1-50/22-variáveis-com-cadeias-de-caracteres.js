1a11d9b6bc6308079bd0c0c63bee1ea7afc0ce73
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 13:19:52 -0300

    Cadeias de caracteres.

---
+++ aulas 1-50/22-variáveis-com-cadeias-de-caracteres.js
@@ -0,0 +1,15 @@
+// Variáveis ​​com cadeias de caracteres em JavaScript.
+var nome = "João";
+console.log(nome);
+// variações
+var nome = "J oao";
+console.log(nome);
+var nome = "Jo@o";
+console.log(nome);
+var nome = "João12345";
+console.log(nome);
+
+
+
+
+
