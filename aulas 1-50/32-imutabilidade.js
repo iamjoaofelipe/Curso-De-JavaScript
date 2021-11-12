7d8599ce31398c9bafdc6245ae51a4c0dff6bebe
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 16:33:33 -0300

    Imutabilidade.

---
+++ aulas 1-50/32-imutabilidade.js
@@ -0,0 +1,11 @@
+// Imutabilidade em JavaScript.
+var escreverErrado = "ulá, Mundo!";
+console.log(escreverErrado);
+escreverErrado[0] = "O";
+console.log(escreverErrado);
+var escreverErrado = "Olá , Mundo!";
+console.log(escreverErrado);
+
+
+
+
