4073f40d78d5c66203447c7ce02036a68d47cc10
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-12 15:25:52 -0300

    Concatenação.

---
+++ aulas 1-50/26-concatenar-cadeias-de-caracteres.js
@@ -0,0 +1,14 @@
+// Concatenação em JavaScript.
+var nomeSobrenome = "João" + " Felipe";
+// Sem espaço
+console.log(nomeSobrenome);
+var nomeSobrenome= "João " + "Felipe";
+// Com espaço
+console.log(nomeSobrenome);
+var nomeSobrenome = "João" + " Felipe";
+// Com espaço
+console.log(nomeSobrenome);
+var nomeSobrenome = "João" + " " +"Felipe"; 
+// Com espaço
+console.log(nomeSobrenome);
+
