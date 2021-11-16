c4673b7b550349e30db6d710d98569805a5ffa89
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 10:52:46 -0300

    Notação,array.

--- aulas 1-50/Notação-array.js
+++ aulas 1-50/Notação-array.js
@@ -2,8 +2,8 @@
 var linguagemDeProgramação = "JavaScript";
 console.log(linguagemDeProgramação);
 /*
-      J a v a S c r i p t
-      0 1 2 3 4 5 6 7 8 9
+J a v a S c r i p t
+0 1 2 3 4 5 6 7 8 9
 */
 console.log(linguagemDeProgramação[0]);
 
