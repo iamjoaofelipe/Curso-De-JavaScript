0a37aa084e99f01219d76a232e5c29d0088102ac
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 09:41:17 -0300

    Notação, Array.

---
+++ aulas 1-50/33-notação-de-parênteses.js
@@ -0,0 +1,25 @@
+// Notação, Array em JavaScript.
+var meuArray = "JavaScript";
+console.log(meuArray);
+/*
+J  a  v  a  S  c  r  i  p  t 
+0  1  2  3  4  5  6  7  8  9    
+*/
+console.log(meuArray[0]);
+console.log(meuArray[1]); 
+console.log(meuArray[2]); 
+console.log(meuArray[3]);
+console.log(meuArray[4]);
+console.log(meuArray[5]);
+console.log(meuArray[6]);
+console.log(meuArray[7]);
+console.log(meuArray[8]);
+console.log(meuArray[9]);
+// exemplo.
+var outroArray = "Programar";
+console.log(outroArray);
+/*
+P  r  o  g  r  a  m  a  r 
+0  1  2  3  4  5  6  7  8   
+*/
+console.log(outroArray[0]);
