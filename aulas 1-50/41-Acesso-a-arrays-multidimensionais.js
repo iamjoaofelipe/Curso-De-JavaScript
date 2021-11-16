a341b80c5bf8808a19dbf24204e0486c3cb8e877
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 14:45:27 -0300

    Array,acessando.

---
+++ aulas 1-50/41-Acesso-a-arrays-multidimensionais.js
@@ -0,0 +1,23 @@
+// acessando arrays em javaScript.
+var meuArray = [[1,2,3] , [4,5,6] , [7,8,9]];
+/*
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+0          1          2
+0  1  2    0  1  2    0  1  2
+*/
+console.log(meuArray[0]); // [1,2,3] 
+console.log(meuArray[1]); // [4,5,6]
+console.log(meuArray[2]); // [7,8,9]
+// acessando as informações do primeiro array.
+console.log(meuArray[0][0]);
+console.log(meuArray[0][1]);
+console.log(meuArray[0][2]);
+// acessando as informações da segunda array.
+console.log(meuArray[1][0]);
+console.log(meuArray[1][1]);
+console.log(meuArray[1][2]);
+// acessando as informações da terceira array.
+console.log(meuArray[2][0]);
+console.log(meuArray[2][1]);
+console.log(meuArray[2][2]);
+
