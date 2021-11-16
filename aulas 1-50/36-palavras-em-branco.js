27726e514cab894d37055730bbbe61b7013ad846
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 10:48:14 -0300

    Palavras em branco.

---
+++ aulas 1-50/36-palavras-em-branco.js
@@ -0,0 +1,8 @@
+// palavras em branco em JavaScript.
+var meuSubstantivo = "Felipe";
+var meuAdjetivo = "está";
+var meuVerbo = "aprendendo";
+var meuAdverbio = "JavaScript"; 
+// concatenar
+var palavrasEmBranco = "O " + meuSubstantivo + " " + meuAdjetivo + " " + meuVerbo + " " + meuAdverbio + " com FreeCodeCamp."; 
+console.log(palavrasEmBranco); 
