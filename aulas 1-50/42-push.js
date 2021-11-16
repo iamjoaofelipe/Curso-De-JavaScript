bd839fa4c2bfe7bf0be495c82601044bf5aa7e47
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 15:12:02 -0300

    Push,adiciona uma ou mais informação.

---
+++ aulas 1-50/42-push.js
@@ -0,0 +1,10 @@
+// O método push() adiciona um ou mais elementos ao final de um array em JavaScript.
+var estações = [["inverno"] , ["outono"] , ["primavera"]];
+console.log(estações);
+estações.push("verão"); // adicionando mais uma informação para a array.
+console.log(estações)  
+
+
+
+
+
