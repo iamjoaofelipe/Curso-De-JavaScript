2cf362820663d7b719f958cbc8350f50a8c67b92
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 14:10:59 -0300

    Array, modificando.

---
+++ aulas 1-50/40-array.js
@@ -0,0 +1,15 @@
+// Modificando as informações da array em JavaScript.
+var meuArray = [10,20,30];
+console.log(meuArray);
+meuArray[0] = 40;
+console.log(meuArray);
+meuArray[1] = "Olá";
+console.log(meuArray[1]);
+meuArray[1] = [1,2,3];
+console.log(meuArray);
+console.log(meuArray[1]); 
+
+
+
+
+
