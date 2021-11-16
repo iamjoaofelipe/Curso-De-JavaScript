20e57e9eaf5b5bef093961b55dbc0276f60761a2
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 10:29:05 -0300

    Último caractere negativo -n.

--- aulas 1-50/35-notação-de-parênteses-da-direita-para-a-esquerda.js
+++ aulas 1-50/35-notação-de-parênteses-da-direita-para-a-esquerda.js
@@ -1,2 +1,15 @@
 // Notação,Array da direita para a esquerda em JavaScript.
+meuArray = "João";
+console.log(meuArray);
+/*
+J  o  ã o  
+-4 - 3 - 2 - 1     
+*/
+// exemplo.
+console.log(meuArray[meuArray.length - 1]);
+// exemplo.
+outroArray = "João";
+n = 4;
+console.log(outroArray[outroArray.length - n])
+
 
