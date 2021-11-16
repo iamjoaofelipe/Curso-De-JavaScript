a43c88be8fdba036feabc2e611e0f853a6da9b5d
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 15:25:35 -0300

    Adiciona informações ao final de um array.

--- aulas 1-50/42-push.js
+++ aulas 1-50/42-push.js
@@ -3,7 +3,8 @@ var estações = [["inverno"] , ["outono"] , ["primavera"]];
 console.log(estações);
 estações.push("verão"); // adicionando mais uma informação para a array.
 console.log(estações)
-  
+
+
 
 
 
