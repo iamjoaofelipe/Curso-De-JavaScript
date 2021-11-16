2f53145fb2e807c1eefdb6399ae2f7a5d3202db0
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 10:17:57 -0300

    Último caractere negativo -1.

--- aulas 1-50/34-notação-array.js
+++ aulas 1-50/34-notação-array.js
@@ -1,11 +1,9 @@
 // Notação, Array, último caractere negativo em JavaScript.
-
 meuArray = "JavaScript";
 console.log(meuArray);
 /*
 J  a  v  a  S  c  r  i  p  t 
 0  1  2  3  4  5  6  7  8  9    
-
 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
 */
 console.log(meuArray[meuArray.length -10]);
