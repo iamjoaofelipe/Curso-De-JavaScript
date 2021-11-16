d06d4b7b61e7efbb74c7b65bb6701ba33f41f96e
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 10:59:21 -0300

    Array.

---
+++ aulas 1-50/37-Array.js
@@ -0,0 +1,9 @@
+// Array em JavaScript.
+var meuNomeIdade = [ "João" , 26];
+console.log(meuNomeIdade);
+var funcionariosBrasilCenter = ["Robson" , "Felipe" , "Rodrigo" , "Pacheco"];
+console.log(funcionariosBrasilCenter);
+var idade = [50 , 30, 39 , 40]; 
+console.log(idade);
+
+
