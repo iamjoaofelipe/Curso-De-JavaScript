238aea5119ddbc39f35bc3789a590e80dd312375
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 15:18:56 -0300

    Pop,deleta a última informação da array.

---
+++ aulas 1-50/43-pop.js
@@ -0,0 +1,9 @@
+// O método pop() remove o último elemento de um array e retorna aquele elemento.
+var estações = [["inverno"] , ["outono"] , ["primavera"] , ["verão"]];
+console.log(estações); 
+estações.pop();
+console.log(estações);
+
+
+
+
