e98c4220b63b30cc306ee62917c5f388f51c2da5
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 15:39:31 -0300

    Adiciona informações ao inicio da array.

--- 45-unshift.js
+++
@@ -1,9 +0,0 @@
-// Manipulação de array com unshift();
-// O método unshift() adiciona um ou mais elementos no início de um array 
-// e retorna o número de elementos (propriedade length) atualizado.
-
-var estacões = ["inverno" , "outono" , "primavera"];
-
-estacões.unshift("verão");
-
-console.log(estacões);
---
+++ aulas 1-50/45-unshift.js
@@ -0,0 +1,10 @@
+// O método unshift() adiciona um ou mais elementos no início de um array 
+var prioridade = ["trabalhar" , "namorar" ,"casar"]
+prioridade.unshift("buscar a Deus");
+console.log(prioridade);
+
+
+
+
+
+
