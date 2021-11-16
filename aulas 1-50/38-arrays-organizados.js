578398618ca4b319da3485819add19f50eedf339
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 11:46:51 -0300

    Arrays organizados.

---
+++ aulas 1-50/38-arrays-organizados.js
@@ -0,0 +1,10 @@
+// Arrays organizados em JavaScript
+var funcionariosBrasilCenter = [["Robson" , 50] , ["Felipe" , 31] , ["Rodrigo" , 45] , ["Pacheco",45]];
+console.log(funcionariosBrasilCenter);
+var meuPassaTempo =[["chocolate" , 5.60 , "brownie"] , ["café" , 2.50 , "pilão"] , ["biscoito" ,5.99 , "maizena"]];
+console.log(meuPassaTempo);
+var ExemploDatas = [[3.4 , 5.6 , 3.2] , [6.7 , 4.5 , 7.8] , [5.6 , 3.4 , 7.8]] 
+console.log(ExemploDatas);
+
+
+
