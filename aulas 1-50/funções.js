f0ae849794c05dc687be44ee9eed6ce7e5330cc7
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 16:49:49 -0300

    Argumentos, concatenação.

---
+++ aulas 1-50/funções.js
@@ -0,0 +1,18 @@
+// Funções , argumentos, concatenação em JavaScript.
+function somar(a, b) {
+    soma = a + b;
+    console.log("O resultado de " + a + " + " + b + " é: " + soma);
+
+}
+somar(5, 3);
+somar(8, 9);
+somar(2, 4);
+somar(5, 5);
+// exemplo 1.
+var x = 5;
+var y = 3;
+somar(x, y);
+function concatenarTres(c1, c2, c3) {
+    console.log(c1 + " " + c2 + " " + c3);
+}
+concatenarTres("Estou" , "aprendendo" , "programação");
