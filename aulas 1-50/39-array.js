9aad3ede300c641078332759c25d0ef3fc7bb36a
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-16 14:04:18 -0300

    Array, somar.

---
+++ aulas 1-50/39-array.js
@@ -0,0 +1,11 @@
+// Acessar, somar array em JavaScript.
+var meuArray = [10 , 20 , 30]; 
+/*
+10 20 30
+0  1   2
+*/
+console.log(meuArray[0]);
+console.log(meuArray[1]);
+console.log(meuArray[2]);
+var somaArray = meuArray[0] + meuArray[1] + meuArray[2];
+console.log(somaArray);
