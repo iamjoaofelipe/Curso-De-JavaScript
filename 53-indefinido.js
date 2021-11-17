8134265fbf9f22260c6414761eeab0f7feb19cbe
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-17 13:21:19 -0300

    Indefinido.

--- 53-indefinido.js
+++
@@ -1,8 +0,0 @@
-// Indefinido em javaScript
-
-function somar(a, b) {
-    console.log(a + b);
-}
-
-// indefinido porque não há declaração de retorno
-console.log(somar)(a, b);
---
+++ aulas 51-100/53-indefinido.js
@@ -0,0 +1,5 @@
+// Valor indefinido em JavaScript.
+function somar(a, b) {
+    console.log(a + b) // ausência de um return.
+}
+console.log(somar(a, b));
