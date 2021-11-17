a165c9a53c44dc20a97afa533d78758668fb49a8
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-17 13:53:49 -0300

    Atribuição de valor.

---
+++ função.js
@@ -0,0 +1,13 @@
+// Atribuir valor função/variável em JavaScript.
+function somar(a,b) {
+    return 5 + 5;
+}
+var resultado = somar (5,5);
+console.log(resultado);
+// exemplo.
+function meuObjetivo(linguagemDeProgramação) {
+    return "Eu quero aprender " + linguagemDeProgramação;
+}
+var minhaMeta = meuObjetivo ("JavaScript.");
+console.log(minhaMeta);
+
