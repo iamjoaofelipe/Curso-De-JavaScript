f112caf4f40166e7622d06ef477bd23c13339da4
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-18 16:09:47 -0300

    Operador e.

--- aulas 51-100/65-operador-menor-ou-igual.js
+++ aulas 51-100/65-operador-menor-ou-igual.js
@@ -1,6 +1,4 @@
 // Operador menor ou igual em JavaScript.
-
-// menor ou igual a que: <=
-
 console.log( 5 < 5); // false
 console.log(5 <= 5); //true
+
---
+++ aulas 51-100/66-operador-logico-e.js
@@ -0,0 +1,24 @@
+/* Operador lógico "e" em JavaScript.
+
+   Tabela da verdade operador E 
+   Para: X && Y
+   | X | Y | X && Y |
+   -----------------------
+   | T | T | T |
+   | T | F | F |
+   | F | T | F |
+   | F | F | F |
+*/
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+var a = 8; 
+// expressões.
+console.log((a > 5) && (a < 10)); // true && true = true.
+a = 10;
+console.log((a >= 5) && (a < 9)); // true && true = false.
+a = 20; // valor alterado.
+console.log((a < 5) && (a > 10)); // false && true = false.
+a = 1;
+console.log(( a == 5) && (a > 10)); // false && false = false
