1b724ea6b6737fa17d6bb7ee702598d8631b1858
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-18 14:10:53 -0300

    Igualdade e estrita.

---
+++ aulas 51-100/59-Pratique-comparar-valores.js
@@ -0,0 +1,21 @@
+// Operador de igualdade e estrita JavaScript.
+var a;
+var b;
+a = 5;
+b = 5;
+console.log(a == b) // true.
+console.log(a === b); // true.
+b = 8; // valor alterado.
+console.log(a == b); // false.
+console.log(a === b); // false.
+b = "5"; // valor alterado.
+console.log(a == b); // true.
+console.log(a === b) // false
+a = "JavaScript"; // valor alterado.
+b = "Javascript"; // valor alterado.
+console.log(a == b); // false
+console.log(a === b); //false.
+
+
+
+
