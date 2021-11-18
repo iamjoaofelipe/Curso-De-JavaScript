74122b829ae1447fc84d4bda67bd33f7437472ae
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-18 16:34:46 -0300

    Operador ou.

---
+++ aulas 51-100/67-operador-ou.js
@@ -0,0 +1,22 @@
+// Operador lógico "ou" em JavaScript.
+/*
+   Tabela da verdade do operador OU 
+   Para: X || Y
+   | X | Y | X || Y |
+   -----------------------
+   | T | T | T |
+   | T | F | T |
+   | F | T | T |
+   | F | F | F |
+ */
+var a = 20;
+console.log(( a > 5) || ( a > 15)) // true.
+a = 30;
+console.log( a > 19) || ( a < 15) // true.
+a = 30;
+console.log( a < 29 ) || ( a > 20 ) // bugou. F,T = true
+a = 5;
+console.log( a === 5 ) || ( a < 15 ) // true.
+
+
+
