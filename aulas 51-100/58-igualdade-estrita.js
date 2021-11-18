5dc25b509165d334537cce5c10e3257daf168546
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-18 13:49:15 -0300

    Operador de igualdade estrita.

--- aulas 51-100/58-igualdade-estrita.js
+++ aulas 51-100/58-igualdade-estrita.js
@@ -1,9 +1,5 @@
 // Operador de igualdade estrita em JavaScript.
-
 console.log(9 == 9); // true
-
 console.log(9 == "9"); // true
-
 console.log(9 === 9); // true
-
 console.log(9 === "9"); // false
