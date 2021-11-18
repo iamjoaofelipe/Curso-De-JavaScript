e05695982d5b4d6e0ed24846015dc0072344a6e9
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-18 13:43:28 -0300

    Operador de igualdade.

---
+++ aulas 51-100/57-igualdade.js
@@ -0,0 +1,10 @@
+// Operador de igualdade em JavaScript.
+console.log(5 == 5); // true.
+console.log(6 == 5); // false
+console.log("Olá" == "Olá"); // true.
+console.log("Olá" == "JavaScript"); // false.
+console.log("Olá" == "olá"); // false
+console.log([1,2,3] == [1,2,3]); // false. Não pode comparar dois objetos na memória.
+console.log([1,2,3] == [4,5,6]); // false.
+
+
