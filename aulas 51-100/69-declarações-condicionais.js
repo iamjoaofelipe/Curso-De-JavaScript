92f9db31b0c99d7b08ebab5d53b21b33d2c8b797
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-19 14:13:27 -0300

    Condicionais.

---
+++ aulas 51-100/69-declarações-condicionais.js
@@ -0,0 +1,17 @@
+// Condicionais em JavaScript.
+if (true) {
+console.log("Essa condição é verdadeira.");   
+}
+var x = 5;
+if (x > 2) {
+console.log("Essa condição é verdadeira.");    
+}
+if (x > 2 && x < 10) {
+console.log("Essa condição é verdadeira.");    
+}
+// exemplo.
+var estação = "inverno";
+if (estação == "inverno") {
+    console.log("Estamos no inverno.");
+}
+
