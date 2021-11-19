431f741c1391aeeaa265aa397d9d085c1c9ae43b
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-19 14:52:04 -0300

    Condicionais.

--- aulas 51-100/72-condicionais.js
+++ aulas 51-100/72-condicionais.js
@@ -1,5 +1,4 @@
 // Condicionais, ordem lógica em Javascript.
-
 function classificarValor(valor) {
     if (valor < 5) {
         console.log("Menor que 5.");
@@ -9,6 +8,6 @@ function classificarValor(valor) {
         console.log("Maior ou igual a 10.");
     }
 }
-classificarValor(7);
-classificarValor(2);
-classificarValor(15);
+classificarValor(4);
+classificarValor(10);
+classificarValor(11);
