7319177f5f099fec928a3616939319cced56553d
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-19 14:29:24 -0300

    Else.

--- aulas 51-100/70-else-if.js
+++ aulas 51-100/70-else-if.js
@@ -6,8 +6,8 @@ if (x < 2) {
     console.log("Essa condição é falsa.");
 }
 // exemplo.
-var estacion = "Invierno";
-if (estacion === "Verano") {
+var estacion = "inverno";
+if (estacion === "verão") {
     console.log("O verão já começou. Agora podemos ir para a praia.");
 } else {
     console.log("Mal posso esperar pelo verão para poder ir à praia.");
