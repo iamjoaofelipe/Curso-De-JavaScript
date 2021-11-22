815b76c9294dd4196c6db43dd03cefaf3ccf3ff0
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-22 13:42:51 -0300

    Else if.

--- aulas 51-100/73-Encadear-instruções-if-else.js
+++ aulas 51-100/73-Encadear-instruções-if-else.js
@@ -1,18 +1,22 @@
 // Encadear instruções if else em JavaScript.
-
 function interpretarIMC(indiceMassaCorporal) {
     if (indiceMassaCorporal < 18.5) {
         console.log("Abaixo do peso.");
     } else if (indiceMassaCorporal <= 24.9){
         console.log("Normal.");
-    }else if (indiceMassaCorporal <= 29.9) {
-        console.log("Acima do peso.");
+    } else if (indiceMassaCorporal <= 29.9) {
+console.log("Acima do peso.");
     } else {
-        console.log("Obeso");
-    }
+  console.log("Obeso.");  
+  }
 }
-
 interpretarIMC(17.8);
 interpretarIMC(22.2);
 interpretarIMC(28.5);
 interpretarIMC(32.2);
+
+
+
+
+
+
