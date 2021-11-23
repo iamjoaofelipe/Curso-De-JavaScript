468906477c6d117f5cf332fe35657ce1e0c534e9
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-23 14:17:16 -0300

    Booleanos.

---
+++ aulas 51-100/79-booleanos.js
@@ -0,0 +1,20 @@
+// Booleanos em JavaScript.
+function éMenorQue(a, b) {
+    if (a < b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// alterações.
+function éMenorQue(a, b) {
+    return a < b;
+}
+console.log(éMenorQue(3, 6)); // true
+console.log(éMenorQue(6, 3)); // false
+console.log(éMenorQue(3, 3)); // false
+
+
+
+
+
