62a951b3edcbc4f23dec2506e6adba5fc90ff085
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-23 16:05:40 -0300

    Adicionando com push.

---
+++ aulas 51-100/86-push.js
@@ -0,0 +1,24 @@
+// push em javaScript.
+var mochila = {
+"cor": "azul",
+"tamanho": "médio",
+"conteúdo": ["garrafa de água" , "caderno"]
+};
+console.log(mochila.cor); // azul
+mochila.cor = "verde";
+console.log(mochila.cor);
+console.log(mochila.conteúdo);
+mochila.conteúdo.push("lápis");
+console.log(mochila.conteúdo);
+
+
+
+
+
+
+
+
+
+  
+
+
