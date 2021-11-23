7db6556c4a3c750eb5c0e6bc8609b92e64e15757
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-23 15:04:22 -0300

    Objetos.

--- aulas 51-100/83-propriedades-de-acesso-notação-de-pontos.js
+++ aulas 51-100/83-propriedades-de-acesso-notação-de-pontos.js
@@ -1,17 +1,12 @@
-// Propriedades de acesso: notação de pontos em JavaScript.
-
+// Objetos em JavaScript.
 var meuCachorro = {
 "nome": "Kiara",
 "idade": 5,
 "peso": 6,
 "raça": "vira-lata",
-
 };
-
 var meuObjeto = {
-
 };
-
 console.log(meuCachorro.nome);
 console.log(meuCachorro.idade);
 console.log(meuCachorro.peso);
