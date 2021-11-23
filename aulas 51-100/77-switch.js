15b0ae9746bec532572b5b42f131a333897f456b
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-23 11:18:07 -0300

    Switch.

--- aulas 51-100/77-switch.js
+++ aulas 51-100/77-switch.js
@@ -1,24 +1,22 @@
-// Sentenças switch multiplos casos em JavaScript.
-
+// Switch em JavaScript.
 function classificarVolume(valor) {
     var volume;
     switch (valor) {
         case 1:
-            volume = "baixo";
+            volume = "Baixo.";
             break;
         case 2:
         case 3:
-            volume = "médio";
+            volume = "Médio.";
             break;
         case 4:
         case 5:
         case 6:
-            volume = "alto";
+            volume = "Alto.";
             break;
     }
     return volume;
 }
-
 console.log(classificarVolume(1));
 console.log(classificarVolume(3));
 console.log(classificarVolume(5));
