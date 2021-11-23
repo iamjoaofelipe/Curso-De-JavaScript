7a571ffe863d2dfaa1605e466e808aff8018d3d3
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-23 15:03:21 -0300

    Objetos.

---
+++ aulas 51-100/82-objetos.js
@@ -0,0 +1,17 @@
+// Objetos em  JavaScript.
+var meuCachorro = {
+  "nome": "kiara",
+  "idade": 5,
+  "peso": 6,
+  "raça": "vira-lata",
+  };
+  var meuObjeto = {
+5: "cinco", 
+  };
+
+
+
+ 
+
+
+
