a96afacd51a630442087693f645934880bd96701
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-24 15:02:56 -0300

    Delete.

---
+++ aulas 51-100/88-delete.js
@@ -0,0 +1,16 @@
+// Eliminar propriedades em um objeto em JavaScript.
+var curso = {
+"titulo": "Aprender JavaScript do zero", 
+"idioma": "português", 
+"duração": 30
+};
+console.log(curso.titulo); 
+delete curso.titulo;
+console.log(curso.titulo); 
+
+
+
+
+
+
+
