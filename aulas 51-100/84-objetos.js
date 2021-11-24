b6739ef065201803c7fe311e3cb159cc506e3133
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-24 14:52:18 -0300

    Assessores, notações de colchetes e pontos.

---
+++ aulas 51-100/84-objetos.js
@@ -0,0 +1,13 @@
+// Assessores de propriedades em JavaScript.
+var meuCaderno = {
+"color": "verde",
+"categoria": 3,
+"número de páginas": 3,
+"número de horas": 100,
+};
+console.log(meuCaderno["color"]); // Notação de colchetes
+console.log(meuCaderno.color); // Notação de ponto.
+
+
+
+  
