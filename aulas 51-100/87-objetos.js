aa0f593f5045f144b4f39738762c53dcbf768b50
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-24 14:48:49 -0300

    Acessores, notações de colchetes e pontos.

---
+++ aulas 51-100/87-objetos.js
@@ -0,0 +1,14 @@
+// Assessores de propriedades em JavaScript.
+var curso = {
+"título": "Aprender JavaScript.",
+"idioma":"pt-br",
+"duração": 30,
+};
+// notação de ponto.
+console.log(curso.título);
+curso.vistas = 34500;
+console.log(curso.vistas); 
+// notação de colchetes.
+curso["vistas"] = 34500; 
+console.log(curso.vistas);
+
