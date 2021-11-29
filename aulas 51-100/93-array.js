44eb0a4604f669159013a26ff39455e60cb6c9d0
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-29 17:35:05 -0300

    Array.

---
+++ aulas 51-100/93-array.js
@@ -0,0 +1,25 @@
+// Array em JavaScript.
+var minhasPlantas = [
+   {
+       tipo: "flores",
+       lista: [
+           "rosas",
+           "túlipas",
+           "dentes-de-leão",
+       ]
+   },
+{
+    tipo: "árvores",
+    lista: [
+        "abeto",
+        "pinheiro",
+        "betula",
+    ]
+}
+];
+var primeiraFlor = minhasPlantas[0].lista[0];
+console.log(primeiraFlor);
+var segundaFlor = minhasPlantas[0].lista[1];
+console.log(segundaFlor);
+var terceiraFlor = minhasPlantas[1].lista[1];
+console.log(terceiraFlor);
