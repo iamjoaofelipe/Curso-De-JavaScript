6a1769396226b9b2764c7e481c473b440ec04f7f
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-30 14:32:29 -0300

    Coleção de discotecas.

--- aulas 51-100/94-coleção-de-album.js
+++ aulas 51-100/94-coleção-de-album.js
@@ -1,5 +1,4 @@
 // Coleção de discotecas em JavaScript.
-
 /*
 Temos um objeto que representa parte de uma coleção de álbuns de música. 
 de álbuns de música. 
@@ -7,20 +6,17 @@ Cada álbum tem um número de identificação único (id) associado a outras pro
 associadas a outras propriedades. 
 Nem todos os álbuns têm informações completas.
 */
-
 var colecãoDeDiscos = {
     7853: {
 
         tituloDoAlbum: "Bee Gees Greatest",
         artista: "Bee Gees",
         canção: ["Stayin 'alive"]
-
     },
     5439: {
         tituloDoAlbum: "ABAA Gold"
     }
 };
-
 /* 
 Defina uma função atualizarDiscos que tome os seguintes parâmetros:
 - discotecas (o objeto que representa a coleção de discotecas).
@@ -55,19 +51,13 @@ function atualizarDiscos(discos, id, propriedade, valor) {
     } else {
 
         discos[id][propriedade] = valor;
-
-
     }
-
 }
-
 console.log(colecãoDeDiscos);
 atualizarDiscos(colecãoDeDiscos, 7853, "tituloDealbum", "");
 console.log(colecãoDeDiscos);
-
 atualizarDiscos(colecãoDeDiscos, 5439, "canção", "mamma mia");
 console.log(colecãoDeDiscos);
-
 atualizarDiscos(colecãoDeDiscos, 5439, "artista", "ABBA");
 console.log(colecãoDeDiscos);
 
