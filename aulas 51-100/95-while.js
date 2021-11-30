943012814786821d37ac98133efc01711b56564d
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-11-30 14:56:16 -0300

    While.

---
+++ aulas 51-100/95-while.js
@@ -0,0 +1,20 @@
+// While em JavaScript.
+var i = 0;
+while (i <= 3) {
+    console.log("Olá,Mundo");
+i++;
+}
+// exemplo 2.
+var meuArray = [];
+meuArray.push(i)
+while (i  < 10) {
+    meuArray.push(i);
+i++;
+}
+console.log(meuArray);
+// exemplo 3.
+var números = [2,3,4,5,6,7,8,34];
+while (números.length > 4 ) {
+    números.pop(); // remover o último número.
+}
+console.log(números);
