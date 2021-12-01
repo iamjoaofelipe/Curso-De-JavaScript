a10ff74e26a86be2087d38f74310aaf29ad8c6cf
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-01 15:02:12 -0300

    For.

---
+++ aulas 51-100/96-ciclo-for.js
@@ -0,0 +1,12 @@
+// "for" em JavaScript.
+var meuArray = [];
+for (var i = 0; i < 10; i++) {
+    meuArray.push(i);
+}
+console.log(meuArray);
+// exemplo 2.
+var meuArray = [];
+for (var i = 0; i < 10; i += 2) {
+    meuArray.push("A");
+}
+console.log(meuArray);
