5f14e46e42babf1f4acb8089802b02928bda63f5
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-02 15:09:21 -0300

    For.

--- aulas 51-100/99-for.js
+++ aulas 51-100/99-for.js
@@ -1,37 +1,27 @@
-// trabalhando com "for" em JavaScript.
-
-// exemplo 1 
-
+// "for" em JavaScript.
 var meuArray = [4, 6, 8, 2];
 var total = 0;
-
 for (var i = 0; i < meuArray.length; i++) {
     total += meuArray[i];
 }
-
 console.log(total);
-
-// exemplo 2
-
+// exemplo 2.
 var linguagemDeProgramação = ["JavaScript", "Python", "Java", "C++"];
-
 for (var i = 0; i < linguagemDeProgramação.length; i++) {
-    console.log(linguagemDeProgramação[i].toUpperCase());
+    console.log(linguagemDeProgramação[i].toUpperCase()); // letras maiúsculas.
 }
-
-// Ejemplo
-
-function contarNumerosPares(acordo) {
+function contarNumerosPares(array) {
     var total = 0;
-    for (var i = 0; i < acordo.length; i++) {
-        if (acordo[i] % 2 == 0) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
             total++;
         }
     }
+
     return total;
 }
-
 console.log(contarNumerosPares([5, 3, 1]));
 console.log(contarNumerosPares([5, 3, 1, 2]));
 console.log(contarNumerosPares([5, 3, 1, 2, 4, 8]));
 console.log(contarNumerosPares([5, 2, 6, 3, 7, 5, 6, 3]));
+
