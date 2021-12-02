f68a4ce93bb2ebec7978ccf330f985bd0f2c4b93
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-02 15:16:02 -0300

    Array.

---
+++ 100-array.js
@@ -0,0 +1,12 @@
+// Array em JavaScript.
+var meuArray = [[1,2,3] , [4,5,6] , [7,8,9]];
+for (var i = 0; i < meuArray.length; i++) {
+   var exemplo = meuArray[i];
+   for (var j = 0; j < meuArray.length; j++) {
+   console.log(exemplo[j]);
+   }
+}
+
+
+
+
