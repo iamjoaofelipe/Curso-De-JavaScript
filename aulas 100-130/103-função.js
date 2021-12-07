45d3a76534af086a943b4fe5a7cb30a0e4d77bf2
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-07 16:39:07 -0300

    Números aleatórios.

---
+++ aulas 100-130/103-função.js
@@ -0,0 +1,11 @@
+// Função em JavaScript.
+function geradorDeFracãoAleatória() {
+    return Math.random();
+}
+console.log(geradorDeFracãoAleatória());
+console.log(geradorDeFracãoAleatória());
+console.log(geradorDeFracãoAleatória());
+console.log(geradorDeFracãoAleatória());
+var númeroAleatório = Math.random();
+console.log(númeroAleatório);
+
