3ef283b0f861a942d6f0bc23cba304ec2ee81df7
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-08 16:16:46 -0300

    Números aleatórios.

---
+++ aulas 100-130/104-função.js
@@ -0,0 +1,16 @@
+// Função em JavaScript.
+var númerosAleatóriosEntre0e19 = Math.floor(Math.random()* 20); 
+function geradorNúmerosAleatórios(limiteSuperior) {
+    return Math.floor(Math.random()* limiteSuperior);
+    }
+    console.log(geradorNúmerosAleatórios(5));
+    console.log(geradorNúmerosAleatórios(5));
+    console.log(geradorNúmerosAleatórios(5));
+    console.log(geradorNúmerosAleatórios(5));
+    console.log(geradorNúmerosAleatórios(5));
+    for (let i = 0; i < 15; i++) {
+    console.log(geradorNúmerosAleatórios(5));
+    }
+    
+    
+
