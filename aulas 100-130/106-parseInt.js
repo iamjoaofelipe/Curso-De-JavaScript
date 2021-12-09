48ba7e52d171eac1828f6540eafe6e7bb4a1367e
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-09 11:31:48 -0300

    Números inteiros.

---
+++ aulas 100-130/106-parseInt.js
@@ -0,0 +1,15 @@
+// Números inteiros em JavaScript.
+console.log(parseInt("5"));
+console.log(parseInt("-47"));
+console.log(parseInt("0"));
+console.log(parseInt("6.7"));
+console.log(parseInt("6.2"));
+console.log(parseInt(6.7));
+console.log(parseInt("abc")); // NaN
+// exemplos com variáveis.
+var a = "5";
+var b = "7";
+console.log(a + b);
+var a = parseInt("5");
+var b = parseInt("7");
+console.log(a + b);
