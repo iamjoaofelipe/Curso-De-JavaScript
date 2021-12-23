99fe59dab5ea9fc5f8ec3d251947be5d146b445f
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-23 14:13:01 -0300

    Funções de setas com parâmetros.

---
+++ aulas 100-130/116-funcao.js
@@ -0,0 +1,23 @@
+// Funções de setas com parâmetros.
+const somarTres = function(x) {
+    return x + 3;
+}
+console.log(somarTres(3));
+// função flexa
+const somarTres1 = (x) => x + 3;
+console.log(somarTres1(4));
+// exemplo
+const concatenar = function(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+console.log(concatenar([1, 2, 3], [4, 5, ]));
+const concatenar1 = function(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+console.log(concatenar1([1, 2, 3], [4, 5]));
+// Exemplo 3: Várias linhas. Você deve incluir um aparelho e retorno
+const somar = (a, b) => {
+    let num = 6;
+    return a + b + num;
+}
+console.log(somar(1, 1));
