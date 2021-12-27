7e7cc2aac98d07ffd27cb6730b4fe87563e07ca2
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2021-12-27 16:47:12 -0300

    Objeto conciso.

---
+++ aulas 100-130/125-objetos.js
@@ -0,0 +1,14 @@
+// Crie objetos de forma concisa em JavaScript.
+var a = 6;
+console.log(`O valor da variavel e ${a}.`);
+var nome = "Joao";
+var idade = 26;
+console.log(`meu nome é ${nome},tenho ${idade} anos de idade.`);
+var meuArray = [1, 2, 3, 4];
+console.log(`Meu array e: ${JSON.stringify(meuArray)}`);
+const funcionario = {
+    nome: "Bruno",
+    idade: 38
+};
+const saudacao = `Olá! meu nome e ${funcionario.nome}, eu tenho ${funcionario.idade} anos de idade.`;
+console.log(saudacao);
